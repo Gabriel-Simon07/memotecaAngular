a92ad9caa511a73373f11ea6abc4a6b8ac173c8d
Gabriel Simon <gabrielsimon775@gmail.com>
2022-11-22 21:46:07 -0300

    mock of pensamento

--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-thought',
   templateUrl: './create-thought.component.html',
   styleUrls: ['./create-thought.component.scss']
 })
-export class CreateThoughtComponent {
+export class CreateThoughtComponent implements OnInit {
 
+  pensamento = {
+    id: 1,
+    conteudo: 'Learn Angular',
+    autoria: 'Dev',
+    modelo: true
+  };
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 }
