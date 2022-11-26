fac05e8b9a3fb0dfa00415f45045fc40fd7d2238
Gabriel Simon <gabrielsimon775@gmail.com>
2022-11-25 22:26:23 -0300

    Router link implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { ThoughtsListComponent } from './components/thoughts/thoughts-list/thoughts-list.component';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listThought',
+    pathMatch: 'full'
+  },
+  {
+    path: 'createThought',
+    component: CreateThoughtComponent
+  },
+  {
+    path: 'listThought',
+    component: ThoughtsListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
